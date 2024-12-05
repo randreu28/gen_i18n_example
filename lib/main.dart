765a240e0d9361451d2f2fdfd008cab928f9ca27
Rubén Andreu <ruben@ecodeliver.tech>
2024-12-05 16:09:09 +0100

    Separates main into a view and main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gen_i18n_example/notifiers/preferences.dart';
+import 'package:gen_i18n_example/views/main_app.dart';
 
 void main() async {
   runApp(const ProviderScope(
@@ -26,61 +27,6 @@ class MyApp extends ConsumerWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
       darkTheme: ThemeData.dark(),
-    );
-  }
-}
-
-class MainApp extends ConsumerWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final preferences = ref.watch(preferencesNotifierProvider);
-    final isDarkMode = preferences.value?.themeMode == ThemeMode.dark;
-
-    void toggleThemeMode(bool isDarkMode) {
-      final preferencesNotifier = ref.read(
-        preferencesNotifierProvider.notifier,
-      );
-
-      preferencesNotifier.setThemeMode(
-        isDarkMode ? ThemeMode.light : ThemeMode.dark,
-      );
-    }
-
-    void switchLocale(Locale locale) {
-      final preferencesNotifier = ref.read(
-        preferencesNotifierProvider.notifier,
-      );
-
-      preferencesNotifier.setLocale(locale);
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.appTitle),
-        actions: [
-          IconButton(
-            icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
-            onPressed: () => toggleThemeMode(isDarkMode),
-          ),
-          PopupMenuButton<Locale>(
-            icon: const Icon(Icons.language),
-            onSelected: switchLocale,
-            itemBuilder: (BuildContext context) {
-              return AppLocalizations.supportedLocales.map((Locale locale) {
-                return PopupMenuItem<Locale>(
-                  value: locale,
-                  child: Text(locale.languageCode.toUpperCase()),
-                );
-              }).toList();
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text(AppLocalizations.of(context)!.appDescription),
-      ),
     );
   }
 }
---
+++ lib/views/main_app.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gen_i18n_example/notifiers/preferences.dart';
+
+class MainApp extends ConsumerWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final preferences = ref.watch(preferencesNotifierProvider);
+    final isDarkMode = preferences.value?.themeMode == ThemeMode.dark;
+
+    void toggleThemeMode(bool isDarkMode) {
+      final preferencesNotifier = ref.read(
+        preferencesNotifierProvider.notifier,
+      );
+
+      preferencesNotifier.setThemeMode(
+        isDarkMode ? ThemeMode.light : ThemeMode.dark,
+      );
+    }
+
+    void switchLocale(Locale locale) {
+      final preferencesNotifier = ref.read(
+        preferencesNotifierProvider.notifier,
+      );
+
+      preferencesNotifier.setLocale(locale);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.appTitle),
+        actions: [
+          IconButton(
+            icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
+            onPressed: () => toggleThemeMode(isDarkMode),
+          ),
+          PopupMenuButton<Locale>(
+            icon: const Icon(Icons.language),
+            onSelected: switchLocale,
+            itemBuilder: (BuildContext context) {
+              return AppLocalizations.supportedLocales.map((Locale locale) {
+                return PopupMenuItem<Locale>(
+                  value: locale,
+                  child: Text(locale.languageCode.toUpperCase()),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(AppLocalizations.of(context)!.appDescription),
+      ),
+    );
+  }
+}
